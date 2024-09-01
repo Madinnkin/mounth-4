098f0fe71e2720b00c0ba8c40fb26ae5b7ba7b4e
Madinnkin <baltabaikyzym@gmail.com>
2024-09-01 19:40:07 +0600

    first home work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function Header () {
+    return (
+    <>
+        <Title/>
+        <h2>I am Header Compenent</h2>
+    </>
+    )
+}
+
+function Footer () {
+    return (
+    <>
+        <Title/>
+        <h2>I am Footer Compenent</h2>
+    </>
+    )
+}
+
+
+function Content () {
+    return (
+        <>
+            <Title/>
+            <h2>I am Content  Compenent</h2>
+        </>
+    )
+}
+
+
+
+function Title (){
+     return (
+        <h2>Title</h2>
+    )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Footer/>
+        <Content/>
     </div>
   );
 }
